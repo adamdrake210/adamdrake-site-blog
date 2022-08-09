209f60c58376eabbca309b23757de03e4b900360
Adam Drake <adam@chemify.io>
2022-08-09 08:33:54 +0200

    generating sitemap

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // eslint-disable-next-line global-require
+      require('./scripts/generate-sitemap');
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
