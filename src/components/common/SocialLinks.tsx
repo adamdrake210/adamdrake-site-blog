ef61aaacbb2217fc40aa5d0388e4a10171020db1
Adam Drake <adam@chemify.io>
2022-08-15 08:56:28 +0200

    adding tooltips for social links

--- src/components/common/SocialLinks.tsx
+++ src/components/common/SocialLinks.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Flex, Icon } from '@chakra-ui/react';
+import { Flex, Icon, Tooltip } from '@chakra-ui/react';
 
 import { GithubLogo } from 'assets/icons/GithubLogo';
 import { TwitterLogo } from 'assets/icons/TwitterLogo';
@@ -16,27 +16,60 @@ export const SocialLinks = () => {
         target="_blank"
         rel="noreferrer"
       >
-        <Icon viewBox="0 0 28 28" width="60px" height="60px" mx={4}>
-          <LinkedInLogo fill={color} />
-        </Icon>
+        <Tooltip label="For recruiters...">
+          <Icon
+            viewBox="0 0 28 28"
+            width="60px"
+            height="60px"
+            mx={4}
+            _hover={{
+              transform: 'scale(1.1);',
+              transition: 'all .2s ease-in-out',
+            }}
+          >
+            <LinkedInLogo fill={color} />
+          </Icon>
+        </Tooltip>
       </a>
       <a
         href="https://github.com/adamdrake210"
         target="_blank"
         rel="noreferrer"
       >
-        <Icon viewBox="0 0 65 65" width={50} height={50} mx={4}>
-          <GithubLogo fill={color} />
-        </Icon>
+        <Tooltip label="For code enthusiasts...">
+          <Icon
+            viewBox="0 0 65 65"
+            width={50}
+            height={50}
+            mx={4}
+            _hover={{
+              transform: 'scale(1.1);',
+              transition: 'all .2s ease-in-out',
+            }}
+          >
+            <GithubLogo fill={color} />
+          </Icon>
+        </Tooltip>
       </a>
       <a
         href="https://twitter.com/FrontEndDrake"
         target="_blank"
         rel="noreferrer"
       >
-        <Icon viewBox="0 0 24 24" width={50} height={50} mx={4}>
-          <TwitterLogo fill={color} />
-        </Icon>
+        <Tooltip label="For the birds...">
+          <Icon
+            viewBox="0 0 24 24"
+            width={50}
+            height={50}
+            mx={4}
+            _hover={{
+              transform: 'scale(1.1);',
+              transition: 'all .2s ease-in-out',
+            }}
+          >
+            <TwitterLogo fill={color} />
+          </Icon>
+        </Tooltip>
       </a>
     </Flex>
   );
--- src/components/homepage/HomepageContainer.tsx
+++ src/components/homepage/HomepageContainer.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Flex, Box } from '@chakra-ui/react';
+import { Flex, Divider } from '@chakra-ui/react';
+
 import HomepageBanner from './HomepageBanner';
 import { sortNumberByPublishedDateRemote } from 'utils/sortNumberByPublishedDateRemote';
 import { HomepageAboutMeBanner } from './HomepageAboutMeBanner';
@@ -19,7 +20,8 @@ export default function HomepageContainer({ articlePosts }: Props) {
   return (
     <Flex mt={[2, 2, 2, 12]} w="100%" direction="column" alignItems="center">
       <HomepageAboutMeBanner />
-      <SocialLinks />
+      <Divider />
+
       {articleArr.length && (
         <HomepageBanner
           post={articleArr[0].data}
@@ -28,6 +30,7 @@ export default function HomepageContainer({ articlePosts }: Props) {
           pageTitle="Latest Article"
         />
       )}
+      <SocialLinks />
     </Flex>
   );
 }
