f0b2a4a446fd04ce6bce12ab87346de44b322c38
Adam Drake <adam@chemify.io>
2022-08-15 08:46:58 +0200

    sorting colors

--- src/components/common/SocialLinks.tsx
+++ src/components/common/SocialLinks.tsx
@@ -4,8 +4,11 @@ import { Flex, Icon } from '@chakra-ui/react';
 import { GithubLogo } from 'assets/icons/GithubLogo';
 import { TwitterLogo } from 'assets/icons/TwitterLogo';
 import { LinkedInLogo } from 'assets/icons/LinkedInLogo';
+import { useThemeColors } from 'hooks/useThemeColors';
 
 export const SocialLinks = () => {
+  const { color } = useThemeColors();
+
   return (
     <Flex my={8}>
       <a
@@ -13,14 +16,8 @@ export const SocialLinks = () => {
         target="_blank"
         rel="noreferrer"
       >
-        <Icon
-          viewBox="0 0 28 28"
-          color="white"
-          width="60px"
-          height="60px"
-          mx={4}
-        >
-          <LinkedInLogo />
+        <Icon viewBox="0 0 28 28" width="60px" height="60px" mx={4}>
+          <LinkedInLogo fill={color} />
         </Icon>
       </a>
       <a
@@ -28,8 +25,8 @@ export const SocialLinks = () => {
         target="_blank"
         rel="noreferrer"
       >
-        <Icon viewBox="0 0 65 65" color="white" width={50} height={50} mx={4}>
-          <GithubLogo />
+        <Icon viewBox="0 0 65 65" width={50} height={50} mx={4}>
+          <GithubLogo fill={color} />
         </Icon>
       </a>
       <a
@@ -37,8 +34,8 @@ export const SocialLinks = () => {
         target="_blank"
         rel="noreferrer"
       >
-        <Icon viewBox="0 0 50 50" color="white" width={50} height={50} mx={4}>
-          <TwitterLogo />
+        <Icon viewBox="0 0 24 24" width={50} height={50} mx={4}>
+          <TwitterLogo fill={color} />
         </Icon>
       </a>
     </Flex>
