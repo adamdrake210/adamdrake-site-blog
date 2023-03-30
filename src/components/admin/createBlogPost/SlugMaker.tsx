bf84c70227b4c49782ecc6e3ba4bb50014058ff7
Adam Drake <adamgedrake@gmail.com>
2023-03-30 08:44:59 +0200

    feat: slug maker (#28)
    * feat: slug maker
    
    * cleanup
    
    * updating type
    
    ---------
    
    Co-authored-by: Adam Drake <adam@chemify.io>

---
+++ src/components/admin/createBlogPost/SlugMaker.tsx
@@ -0,0 +1,45 @@
+import { Box, Button, Input, Title } from '@mantine/core';
+import { TextCopier } from 'components/common/TextCopier';
+import React from 'react';
+
+export const SlugMaker = () => {
+  const [slug, setSlug] = React.useState<string>('');
+  const [title, setTitle] = React.useState<string>('');
+
+  const convertTitleToSlug = () => {
+    const slug = title
+      .toLowerCase()
+      .replace(/ /g, '-')
+      .replace(/[^\w-]+/g, '');
+    setSlug(slug);
+  };
+
+  return (
+    <Box>
+      <Title order={1} fz={36}>
+        Convert Title to Slug
+      </Title>
+      <Input.Wrapper
+        description="This is the slug that will be used in the URL"
+        label="Title"
+        w={600}
+      >
+        <Input
+          name="title"
+          value={title}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setTitle(e.target.value)
+          }
+          placeholder="Enter title here"
+          w="100%"
+        />
+      </Input.Wrapper>
+      <Box my={16}>
+        <Button w={150} onClick={convertTitleToSlug}>
+          Convert
+        </Button>
+      </Box>
+      {slug && <TextCopier textToCopy={slug} title="Blog Post Slug" />}
+    </Box>
+  );
+};
