f94257a8eb3caad54e6246c8f95cbadd8640ef4a
Adam Drake <adam@chemify.io>
2023-04-10 08:57:55 +0200

    cleanup

--- src/services/api/cloudinaryApi.ts
+++ src/services/api/cloudinaryApi.ts
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 export const postExistingImageToCloudinary = async (imageUrl: any) => {
   const response = await fetch('/api/cloudinary/uploadImage', {
     method: 'POST',
@@ -7,27 +5,6 @@ export const postExistingImageToCloudinary = async (imageUrl: any) => {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({ imageUrl, width: 960, height: 560 }),
-  });
-
-  if (!response.ok) {
-    throw new Error(
-      `Failed to upload image to Cloudinary: ${response.statusText}`,
-    );
-  }
-  return response.json();
-};
-
-export const uploadFileImageToCloudinary = async (payload: any) => {
-  console.log(
-    '🚀 ~ file: cloudinaryApi.ts:21 ~ uploadFileImageToCloudinary ~ payload:',
-    payload,
-  );
-  const response = await fetch('/api/cloudinary/uploadImageFile', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ payload }),
   });
 
   if (!response.ok) {
