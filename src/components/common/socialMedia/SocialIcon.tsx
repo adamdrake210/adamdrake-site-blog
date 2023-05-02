9538501b13e717ebfaedc238ee4843c643b32d1b
Adam Drake <adam@chemify.io>
2023-05-02 08:56:07 +0200

    social media icons refactor

---
+++ src/components/common/socialMedia/SocialIcon.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { ActionIcon, Tooltip } from '@mantine/core';
+
+type Props = {
+  label: string;
+  icon: React.ReactNode;
+  href: string;
+};
+
+export const SocialIcon = ({ label, icon, href }: Props) => {
+  return (
+    <a href={href} target="_blank" rel="noreferrer">
+      <Tooltip label={label}>
+        <ActionIcon
+          variant="transparent"
+          w={60}
+          h={60}
+          mx={32}
+          sx={{
+            '&:hover': {
+              transform: 'scale(1.1);',
+              transition: 'all .2s ease-in-out',
+            },
+          }}
+        >
+          {icon}
+        </ActionIcon>
+      </Tooltip>
+    </a>
+  );
+};
--- src/components/common/socialMedia/SocialLinks.tsx
+++ src/components/common/socialMedia/SocialLinks.tsx
@@ -1,84 +1,35 @@
 import React from 'react';
+import { Flex } from '@mantine/core';
 
 import { GithubLogo } from 'assets/icons/GithubLogo';
 import { TwitterLogo } from 'assets/icons/TwitterLogo';
 import { LinkedInLogo } from 'assets/icons/LinkedInLogo';
-import { ActionIcon, Flex, Tooltip, useMantineTheme } from '@mantine/core';
+import { SocialIcon } from './SocialIcon';
+
+const socialIconsData = [
+  {
+    label: 'For recruiters...',
+    icon: <LinkedInLogo />,
+    href: 'https://www.linkedin.com/in/adam-drake-ab065417/',
+  },
+  {
+    label: 'For code enthusiasts...',
+    icon: <GithubLogo />,
+    href: 'https://github.com/adamdrake210',
+  },
+  {
+    label: 'For the birds...',
+    icon: <TwitterLogo />,
+    href: 'https://twitter.com/FrontEndDrake',
+  },
+];
 
 export const SocialLinks = () => {
-  const theme = useMantineTheme();
-
-  const iconColor =
-    theme.colorScheme === 'dark' ? theme.colors.gray[1] : theme.colors.gray[6];
-
   return (
     <Flex my={32}>
-      <a
-        href="https://www.linkedin.com/in/adam-drake-ab065417/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <Tooltip label="For recruiters...">
-          <ActionIcon
-            w={60}
-            h={60}
-            mx={32}
-            sx={{
-              '&:hover': {
-                transform: 'scale(1.1);',
-                transition: 'all .2s ease-in-out',
-              },
-            }}
-            color={iconColor}
-          >
-            <LinkedInLogo fill={iconColor} />
-          </ActionIcon>
-        </Tooltip>
-      </a>
-      <a
-        href="https://github.com/adamdrake210"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <Tooltip label="For code enthusiasts...">
-          <ActionIcon
-            w={60}
-            h={60}
-            mx={32}
-            sx={{
-              '&:hover': {
-                transform: 'scale(1.1);',
-                transition: 'all .2s ease-in-out',
-              },
-            }}
-            color={iconColor}
-          >
-            <GithubLogo fill={iconColor} />
-          </ActionIcon>
-        </Tooltip>
-      </a>
-      <a
-        href="https://twitter.com/FrontEndDrake"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <Tooltip label="For the birds...">
-          <ActionIcon
-            w={60}
-            h={60}
-            mx={32}
-            sx={{
-              '&:hover': {
-                transform: 'scale(1.1);',
-                transition: 'all .2s ease-in-out',
-              },
-            }}
-            color={iconColor}
-          >
-            <TwitterLogo fill={iconColor} />
-          </ActionIcon>
-        </Tooltip>
-      </a>
+      {socialIconsData.map(({ label, icon, href }) => (
+        <SocialIcon key={label} label={label} icon={icon} href={href} />
+      ))}
     </Flex>
   );
 };
