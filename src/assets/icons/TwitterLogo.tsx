ff96b307987ba7560acc5e6e663905b8a7f97d6f
Adam Drake <adam@chemify.io>
2024-01-13 11:35:49 +0100

    fe: cleanup

--- src/assets/icons/TwitterLogo.tsx
+++ src/assets/icons/TwitterLogo.tsx
@@ -13,11 +13,8 @@ export const TwitterLogo = () => {
       width="54"
       height="54"
       viewBox="0 0 24 24"
-      stroke-width="2"
       stroke="currentColor"
       fill={iconColor}
-      stroke-linecap="round"
-      stroke-linejoin="round"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M4 4l11.733 16h4.267l-11.733 -16z" />
